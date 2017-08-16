fdb45994140eecaf613c627bed7520f49c96f046
Michelle Oliveira <michelleolivcosta@gmail.com>
2017-08-16 13:04:19 -0300

    Fazer: #test de thread, #dockerfile client e server #docker-compose

---
+++ questao-04/client/src/test/java/br/edu/ifpb/client/PersistUserTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.client;
+
+import br.edu.ifpb.shared.Usuario;
+import com.sun.istack.internal.logging.Logger;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.junit.Test;
+
+/**
+ *
+ * @author miolivc
+ */
+public class PersistUserTest {
+    
+    private PersistUser persist = new PersistUser();
+    
+    @Test
+    public void timeFor100Users() {
+        Usuario usuario = new Usuario("Teste 100 usuarios");
+        LocalTime initialTime = LocalTime.now();
+        for (int i = 0; i < 100; i++) {
+            persist.persistUser(usuario);
+        }
+        LocalTime finalTime = LocalTime.now();
+        Logger.getLogger(PersistUser.class).info("Tempo para 100 usuarios: " +
+                (finalTime.minusSeconds(initialTime.getSecond())));
+    }
+    
+    @Test
+    public void timeFor100UsersLoop() {
+        Usuario usuario = new Usuario("Teste 1000 usuarios loop");
+        LocalTime initialTime = LocalTime.now();
+        for (int i = 0; i < 1000; i++) {
+            persist.persistUser(usuario);
+        }
+        LocalTime finalTime = LocalTime.now();
+        Logger.getLogger(PersistUser.class).info("Tempo para 1000 usuarios com loop: " +
+                (finalTime.minusSeconds(initialTime.getSecond())));
+    }
+    
+    @Test
+    public void timeFor100UsersThread() {
+        
+    }
+}
